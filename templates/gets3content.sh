da4a0a3b5c36f07fd75980b6b8a7b5b098478bd3
Duncan Mountford <duncan9562@gmail.com>
2019-03-25 11:45:20 +0000

    Change user_data to env var

--- templates/gets3content.sh
+++ templates/gets3content.sh
@@ -3,8 +3,8 @@
 set -e
 
 # Obtain HTTPD SSL configuration from s3 bucket
-aws s3 cp s3://$(curl 169.254.169.254/latest/user-data)/httpd.conf /etc/httpd/conf/httpd.conf --region eu-west-2
-aws s3 cp s3://$(curl 169.254.169.254/latest/user-data)/ssl.conf /etc/httpd/conf.d/ssl.conf --region eu-west-2
+aws s3 cp s3://$s3_bucket_name/httpd.conf /etc/httpd/conf/httpd.conf --region eu-west-2
+aws s3 cp s3://$s3_bucket_name/latest/user-data)/ssl.conf /etc/httpd/conf.d/ssl.conf --region eu-west-2
 
 # Reload HTTPD
 sudo systemctl reload httpd
