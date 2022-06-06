4ad6d82ccceef24859b804e68a256b9e755f35dd
JWanderema <wanderema@hotmail.co.uk>
2022-06-06 15:35:30 +0100

    test raw

--- gets3content/gets3content.py
+++ gets3content/gets3content.py
@@ -13,32 +13,49 @@ CONFIG = Config(
     )
 )
 
-s3 = boto3.resource('s3', config=CONFIG, region_name='eu-west-2')
 
-def gets3content():
-    s3_bucket_name = os.getenv('s3_bucket_name')
 
-    try:
-        s3.Bucket(s3_bucket_name).download_file('ssl.conf', '/etc/httpd/conf.d/ssl.conf')
-    except botocore.exceptions.ClientErrors as e:
-        if e.response['Error']['Code'] == '404':
-            logging.info('The Object does not exist')
-        else:
-            raise
+def check_python_v():
 
     try:
-        s3.Bucket(s3_bucket_name).download_file('httpd.conf', '/etc/httpd/conf/httpd.conf')
-    except botocore.exceptions.ClientErrors as e:
-        if e.response['Error']['Code'] == '404':
-            logging.info('The Object does not exist')
-        else:
-            raise
-    try:
-        os.system("sudo systemctl reload httpd")
+        version = os.system("/usr/src/Python-3.7.13/python -V")
+        return
+
+        print(version)
     except botocore.exceptions.ClientErrors as e:
         if e.response['Error']['Code'] == '503':
             logging.info('Service Reload Failed')
         else:
             raise
 
-gets3content()
+check_python_v()
+
+# def upgrade_python():
+#
+# def gets3content():
+#     s3_bucket_name = os.getenv('s3_bucket_name')
+#
+#     try:
+#         s3.Bucket(s3_bucket_name).download_file('ssl.conf', '/etc/httpd/conf.d/ssl.conf')
+#     except botocore.exceptions.ClientErrors as e:
+#         if e.response['Error']['Code'] == '404':
+#             logging.info('The Object does not exist')
+#         else:
+#             raise
+#
+#     try:
+#         s3.Bucket(s3_bucket_name).download_file('httpd.conf', '/etc/httpd/conf/httpd.conf')
+#     except botocore.exceptions.ClientErrors as e:
+#         if e.response['Error']['Code'] == '404':
+#             logging.info('The Object does not exist')
+#         else:
+#             raise
+#     try:
+#         os.system("sudo systemctl reload httpd")
+#     except botocore.exceptions.ClientErrors as e:
+#         if e.response['Error']['Code'] == '503':
+#             logging.info('Service Reload Failed')
+#         else:
+#             raise
+#
+# gets3content()
