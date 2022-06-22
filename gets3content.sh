5c135a213481f6d1a251a8ae531e15550713c58c
JWanderema <wanderema@hotmail.co.uk>
2022-06-22 11:12:00 +0100

    remove astrix

--- gets3content.sh
+++ gets3content.sh
@@ -3,8 +3,8 @@
 set -e
 
 # Obtain HTTPD SSL configuration from s3 bucket
-/usr/local/bin/aws s3 cp s3://$s3_bucket_name/httpd.conf /etc/httpd/conf/httpd.conf --region eu-west-2
-/usr/local/bin/aws s3 cp s3://$s3_bucket_name/ssl.conf /etc/httpd/conf.d/ssl.conf --region eu-west-2
+/bin/aws s3 cp s3://$s3_bucket_name/httpd.conf /etc/httpd/conf/httpd.conf --region eu-west-2
+/bin/aws s3 cp s3://$s3_bucket_name/ssl.conf /etc/httpd/conf.d/ssl.conf --region eu-west-2
 
 # Reload HTTPD
 systemctl reload httpd
