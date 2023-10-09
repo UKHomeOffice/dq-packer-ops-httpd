915efe5af0730c19629b0f7d57ead021a23f9d78
Srikanth Arunachalam <116078089+s-arunachalam@users.noreply.github.com>
2023-10-09 10:51:34 +0100

    Yel 8018a: Add multiple public keys (#41)

--- startcloudwatchlogs.sh
+++ startcloudwatchlogs.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Get the instance region and inject it in the conf
-EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 3600"`
+EC2_AVAIL_ZONE=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone)
+
 EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//'`"
 sed -i -e 's/.*region.*/region = '$EC2_REGION'/' /var/awslogs/etc/aws.conf
 
